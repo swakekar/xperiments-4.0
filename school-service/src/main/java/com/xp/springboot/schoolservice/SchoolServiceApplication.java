5879ebc56a0f38617625247f18ce962b8818778a
shreyanswakekar <shreyans.wakekar@gmail.com>
2020-10-01 23:49:02 +0530

    Added school-service

---
+++ school-service/src/main/java/com/xp/springboot/schoolservice/SchoolServiceApplication.java
@@ -0,0 +1,15 @@
+package com.xp.springboot.schoolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SchoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolServiceApplication.class, args);
+	}
+
+}
